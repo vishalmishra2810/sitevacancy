d81d03b86a7479f77118d8f3e3cfd7a5e1c4e8f5
Admin <admin@Admins-MacBook-Pro.local>
2023-03-07 17:33:41 +0530

    create category page

---
+++ components/dashboard/sections/catergorySection/constant.ts
@@ -0,0 +1,37 @@
+export const CATEGORY_SECTION = [
+  {
+    title: "FrontEnd Developer",
+    description: 200,
+    color: "#FA4F08",
+  },
+  {
+    title: "FrontEnd Developer",
+    description: 200,
+    color: "#6736C3",
+  },
+  {
+    title: "FrontEnd Developer",
+    description: 200,
+    color: "#FEB813",
+  },
+  {
+    title: "FrontEnd Developer",
+    description: 200,
+    color: "#005BF9",
+  },
+  {
+    title: "FrontEnd Developer",
+    description: 200,
+    color: "#005BF9",
+  },
+  {
+    title: "FrontEnd Developer",
+    description: 200,
+    color: "#FEB813",
+  },
+  {
+    title: "FrontEnd Developer",
+    description: 200,
+    color: "#6736C3",
+  },
+];
