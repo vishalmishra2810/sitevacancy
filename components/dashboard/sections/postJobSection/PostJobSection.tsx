31a7f8a11f98ff77d9bc0a0f763a4596ae15543d
Admin <admin@Admins-MacBook-Pro.local>
2023-03-13 11:18:01 +0530

    job list page details

--- components/dashboard/sections/postJobSection/PostJobSection.tsx
+++ components/dashboard/sections/postJobSection/PostJobSection.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import style from "./PostJobSection.module.scss";
-import postJob from "../../../../assets/postJob.png";
+import postJob from "../../../../assets/postjob.png";
 
 function PostJobSection() {
   return (
