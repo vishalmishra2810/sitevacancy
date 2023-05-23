972a00c261c470759ffd9c75b4de5e0fd00f3831
Admin <admin@Admins-MacBook-Pro.local>
2023-05-23 22:36:55 +0530

    adding footer

--- common/footer/Footer.tsx
+++ common/footer/Footer.tsx
@@ -1,38 +1,62 @@
-import React from "react";
-import style from "./Footer.module.scss";
-
-function Footer() {
+import styles from "./Footer.module.scss";
+const Footer = () => {
   return (
-    <div className={style.footer}>
-      <div className={style.footer__top}>
-        <div className={style.footer__top__content}>
-          <ul className={style.footer__top__content__list}>
-            <li className={style.footer__top__content__list__item}>About Us</li>
-            <li className={style.footer__top__content__list__item}>
-              Contact Us
-            </li>
-            <li className={style.footer__top__content__list__item}>
-              Privacy Policy
-            </li>
-            <li className={style.footer__top__content__list__item}>
-              Terms & Conditions
-            </li>
-            <li className={style.footer__top__content__list__item}>FAQ</li>
+    <footer className={styles.footer}>
+      <div className={styles.topSection}>
+        <div className={styles.topSection_left}>
+          <h3>Sign up for our Newsletter</h3>
+          <p className={styles.topSection_left_p}>
+            Stay informed about our latest updates through email. Subscribe to
+            our newsletter.
+          </p>
+        </div>
+        <div className={styles.newsletter}>
+          <input type="email" placeholder="Enter your email" 
+          className={styles.newsletter_input}/>
+          <button
+            className={styles.newsletter_button}
+          >Subscribe</button>
+        </div>
+      </div>
+      <div className={styles.middleSection}>
+        <div className={styles.column}>
+          <h3>Column 1</h3>
+          <ul>
+            <li>Link 1</li>
+            <li>Link 2</li>
+            <li>Link 3</li>
           </ul>
         </div>
-        <div className={style.footer__bottom__content}>
-          <span className={style.footer__bottom__content__facebook_icon}>
-            <i className="fab fa-facebook-f"></i>
-          </span>
+        <div className={styles.column}>
+          <h3>Column 2</h3>
+          <ul>
+            <li>Link 4</li>
+            <li>Link 5</li>
+            <li>Link 6</li>
+          </ul>
+        </div>
+        <div className={styles.column}>
+          <h3>Column 3</h3>
+          <ul>
+            <li>Link 7</li>
+            <li>Link 8</li>
+            <li>Link 9</li>
+          </ul>
+        </div>
+        <div className={styles.column}>
+          <h3>Column 4</h3>
+          <ul>
+            <li>Link 10</li>
+            <li>Link 11</li>
+            <li>Link 12</li>
+          </ul>
         </div>
       </div>
-      <div className={style.footer__bottom}>
-        <p className={style.footer__bottom__text}>
-          @ 2023 JobPao All Rights Reserved
-        </p>
+      <div className={styles.bottomSection}>
+        <p>&copy; 2023 JobPao. All Rights Reserved.</p>
       </div>
-    </div>
+    </footer>
   );
-}
+};
 
 export default Footer;
