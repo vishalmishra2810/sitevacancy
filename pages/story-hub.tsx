208053ff7109533c258af2d5835144130b703d4d
Vishal Mishra <mishravishal2810@gmail.com>
2023-09-11 06:31:00 +0530

    new changes story

---
+++ pages/story-hub.tsx
@@ -0,0 +1,15 @@
+import Header from "@/common/header/Header";
+import FAQ from "@/components/faq/FAQ";
+import StoryHub from "@/components/story/story";
+import React from "react";
+
+function faq() {
+  return (
+    <>
+      <Header />
+      <StoryHub />
+    </>
+  );
+}
+
+export default faq;
